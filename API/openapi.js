ca9bca420811cd309730bb3df052eec6e3126033
tugumeandree <a1989@A1989s-MacBook-Pro.local>
2023-03-30 19:14:16 +0300

    connected openapi

--- API/openapi.js
+++ API/openapi.js
@@ -1,8 +1,12 @@
-const apiKey = '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';
-const apiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+const apiKey = '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';
+const apiUrl = 'https://api.openai.com/v1/chat/completions';
 
 const messageInput = document.getElementById('message-input');
 const chatOutput = document.getElementById('chat-output');
+const chatOutput1 = document.getElementById('chat-output1');
+const chatOutput2 = document.getElementById('chat-output2');
+const submitButton1 = document.getElementById('submit1');
+const submitButton2 = document.getElementById('submit2');
 
 function sendMessage() {
   const message = messageInput.value;
@@ -14,7 +18,9 @@ function sendMessage() {
       'Authorization': `Bearer ${apiKey}`
     },
     body: JSON.stringify({
-      prompt: `Conversation with ChatGPT:\nUser: ${message}\nChatGPT:`
+        model: "gpt-3.5-turbo",
+  messages: [{role: "user", content: "Hello"}],
+     // prompt: `Conversation with ChatGPT:\nUser: ${message}\nChatGPT:`
     })
   })
   .then(response => response.json())
@@ -24,6 +30,64 @@ function sendMessage() {
   })
   .catch(error => console.error(error));
 
-  messageInput.value = '';
+  //messageInput.value = '';
 }
 
+function getMessage1() {
+  
+    const message1 = submitButton1.value;
+  
+    fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`
+      },
+      body: JSON.stringify({
+model: "gpt-3.5-turbo",
+  messages: [{role: "user", content: "Hello"}],
+       // prompt: `Conversation with ChatGPT:\nUser: ${message1}\nChatGPT:`
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      //const chatbotResponse = data.choices[0].text.trim();
+   chatOutput1.textContent =  data.choices[0].message.content;
+  // chatOutput1.innerHTML += `User: ${message}<br>ChatGPT: ${chatbotResponse}<br>`;
+
+      console.log(data);
+    })
+    .catch(error => console.error(error));
+  
+   // messageInput.value = '';
+  }
+
+  function getMessage2() {
+    
+    const message2 = submitButton2.value;
+  
+    fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+  messages: [{role: "user", content: "Hello"}],
+     //   prompt: `Conversation with ChatGPT:\nUser: ${message2}\nChatGPT:`
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+     // const chatbotResponse = data.choices[0].text.trim();
+    //  chatOutput2.innerHTML += `User: ${message}<br>ChatGPT: ${chatbotResponse}<br>`;
+      console.log(data);
+    })
+    .catch(error => console.error(error));
+  
+    //messageInput.value = '';
+  }
+
+  submitButton1.addEventListener('click',getMessage1);
+  submitButton2.addEventListener('click',getMessage2);
